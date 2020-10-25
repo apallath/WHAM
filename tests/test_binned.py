Updated tests for binned self-consistent solver (correct), working on issues with binned log-likelihood

tests/test_binned.py
@@ -10,15 +10,16 @@ from WHAM.lib.potentials import harmonic
 import WHAM.binned
 
 
-def test_binned_Nt_self_consistent_stat_ineff():
+def tes_binned_Nt_self_consistent_stat_ineff():
     # N* associated with each window
     n_star_win = [30, 25, 20, 15, 10, 5, 0, -5]
     # kappa associated with each window
     kappa_win = [0.98] * len(n_star_win)
     # Umbrella potentials (applied on CV) from each window
     # In this case, the potential was a harmonic potential, kappa/2 (N - N*)^2
-    umbrella_win = []
-    for i in range(len(n_star_win)):
+    # For the unbiased window, the umbrella potential is 0.
+    umbrella_win = [lambda x: 0]
+    for i in range(1, len(n_star_win)):
         kappa = kappa_win[i]
         n_star = n_star_win[i]
         umbrella_win.append(harmonic(kappa, n_star))
@@ -47,23 +48,20 @@ def test_binned_Nt_self_consistent_stat_ineff():
                     vals = line.strip().split()
                     Ntw_i.append(float(vals[2]))
             Ntw_i = np.array(Ntw_i)
-            Ntw_win.append(Ntw_i[500:])
-
-    for i in range(len(n_star_win)):
-        print(np.min(Ntw_win[i]), np.max(Ntw_win[i]))
+            Ntw_win.append(Ntw_i[200:])
 
     beta = 1000 / (8.314 * 300)  # at 300 K, in kJ/mol units
 
     # Perform WHAM calculation
     betaF_l = WHAM.binned.compute_betaF_profile(Ntw_win, bin_centers,
                                                 umbrella_win, beta, solver='self-consistent',
-                                                scale_stat_ineff=True, tol=1e-12)
+                                                scale_stat_ineff=True, tol=1e-10)
 
-    betaF_l = betaF_l - betaF_l[30]  # resposition zero so that unbiased free energy is zero
+    betaF_l = betaF_l - betaF_l[30]  # reposition zero so that unbiased free energy is zero
 
     # Plot
     fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
-    ax.plot(bin_centers, betaF_l, label="Solution")
+    ax.plot(bin_centers, betaF_l, label="Self-consistent WHAM solution")
 
     bin_centers_ref = np.load("seanmarks_ref/bins.npy")
     betaF_l_ref = np.load("seanmarks_ref/betaF_Ntilde.npy")
@@ -72,21 +70,22 @@ def test_binned_Nt_self_consistent_stat_ineff():
     plt.savefig("self_consistent_stat_ineff.png")
 
 
-def test_binned_Nt_bootstrap():
+def test_binned_Nt_log_likelihood_stat_ineff():
     # N* associated with each window
     n_star_win = [30, 25, 20, 15, 10, 5, 0, -5]
     # kappa associated with each window
     kappa_win = [0.98] * len(n_star_win)
     # Umbrella potentials (applied on CV) from each window
     # In this case, the potential was a harmonic potential, kappa/2 (N - N*)^2
-    umbrella_win = []
-    for i in range(len(n_star_win)):
+    # For the unbiased window, the umbrella potential is 0.
+    umbrella_win = [lambda x: 0]
+    for i in range(1, len(n_star_win)):
         kappa = kappa_win[i]
         n_star = n_star_win[i]
         umbrella_win.append(harmonic(kappa, n_star))
 
     # List of bins to perform binning into
-    bin_centers = np.array(list(range(35)))
+    bin_centers = np.linspace(0, 34, 34 + 1)
 
     # Raw, correlated timeseries CV data from each window
     Ntw_win = []
@@ -109,25 +108,28 @@ def test_binned_Nt_bootstrap():
                     vals = line.strip().split()
                     Ntw_i.append(float(vals[2]))
             Ntw_i = np.array(Ntw_i)
-            Ntw_win.append(Ntw_i[500:])
+            Ntw_win.append(Ntw_i[200:])
 
     beta = 1000 / (8.314 * 300)  # at 300 K, in kJ/mol units
 
     # Perform WHAM calculation
-    betaF_l, betaF_l_berr = WHAM.binned.bootstrap_betaF_profile(100, Ntw_win, bin_centers,
-                                                                umbrella_win, beta, solver='self-consistent',
-                                                                track_progress=True, tol=1e-12)
+    betaF_l, status = WHAM.binned.compute_betaF_profile(Ntw_win, bin_centers,
+                                                        umbrella_win, beta, solver='log-likelihood',
+                                                        scale_stat_ineff=True, opt_method='nelder-mead')
 
-    betaF_l = betaF_l - betaF_l[30]  # resposition zero so that unbiased free energy is zero
+    print(status)
 
+    betaF_l = betaF_l - betaF_l[30]  # reposition zero so that unbiased free energy is zero
+
+    # Plot
     fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
-    ax.errorbar(bin_centers, betaF_l, yerr=betaF_l_berr, capsize=3, label="Solution")
+    ax.plot(bin_centers, betaF_l, label="Log-likelihood WHAM solution")
 
     bin_centers_ref = np.load("seanmarks_ref/bins.npy")
     betaF_l_ref = np.load("seanmarks_ref/betaF_Ntilde.npy")
     ax.plot(bin_centers_ref, betaF_l_ref, label="Reference (Sean Marks)")
     ax.legend()
-    plt.savefig("self_consistent_bootstrap.png")
+    plt.savefig("log_likelihood_stat_ineff.png")
 
 
 if __name__ == "__main__":